4ed4d863027ab78fffbb6bf195fd1ad5d287b08d
Alexandr Azizyan <alex.r.azizian@gmail.com>
2016-05-04 10:38:01 +0400

    Add junit test with Mockito

--- src/test/java/LoggerTest.java
+++ src/test/java/LoggerTest.java
@@ -3,6 +3,10 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class LoggerTest {
 
@@ -13,5 +17,18 @@ public class LoggerTest {
         target.log("Hello, Travis-CI !!!");
 
         assertThat(helper.toString(), is(equalTo("LogInfo: Hello, Travis-CI !!!\n")));
+    }
+
+    @Test
+    public void testLog_withMockedHelper() {
+        LoggingHelper helper = mock(LoggingHelper.class);
+        Logger target = new Logger(helper);
+        target.log("A");
+        target.log("B");
+
+        verify(helper, times(2)).flush();
+        verify(helper).writeLine("LogInfo: A\n");
+        verify(helper).writeLine("LogInfo: B\n");
+        verify(helper, times(2)).writeLine(anyString());
     }
 }
